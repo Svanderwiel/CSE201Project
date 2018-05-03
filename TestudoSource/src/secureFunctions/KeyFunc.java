2ac21db7775ca1c4d92a251d81edcc1468f1aedb
kayrf <kayrf@miamioh.edu>
2018-05-03 03:19:38 -0400

    added key functions

---
+++ TestudoSource/src/secureFunctions/KeyFunc.java
@@ -0,0 +1,31 @@
+package secureFunctions;
+
+import java.math.BigInteger;
+
+import org.bitcoinj.core.Address;
+import org.bitcoinj.core.Base58;
+import org.bitcoinj.core.ECKey;
+
+public class KeyFunc {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static byte[] privateKeyGen() {
+		ECKey newkey = new ECKey();
+		return newkey.getPrivKeyBytes(); 
+	}
+	
+	public static byte[] publicKeyGen(BigInteger privKey) {
+		ECKey newkey = ECKey.fromPrivate(privKey);
+		return newkey.getPubKey();
+	}
+	
+	public static String[] toColdStorage(ECKey keyPair){
+		String[] coldKeys = { keyPair.getPrivateKeyAsHex(), keyPair.getPublicKeyAsHex()};
+		return coldKeys;
+	}
+	
+	
+}
